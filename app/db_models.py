5197446ff705da1391e183a35b9b53705b217724
Zac <zpaulson001@gmail.com>
2024-09-07 18:30:40 -0700

    add alembic

--- app/db_models.py
+++ app/db_models.py
@@ -6,5 +6,5 @@ from .database import Base
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(unique=True)
