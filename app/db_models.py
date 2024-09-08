3fa1fc7a733ef2b0faea2252fea5d397d62ae918
Zac <zpaulson001@gmail.com>
2024-09-07 19:43:19 -0700

    add user_setting and language tables

--- app/db_models.py
+++ app/db_models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .database import Base
@@ -8,3 +9,19 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(unique=True)
+
+
+class UserSettings(Base):
+    __tablename__ = "user_settings"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), unique=True)
+    current_language: Mapped[int] = mapped_column(ForeignKey("language.id"), default=1)
+
+
+class Language(Base):
+    __tablename__ = "language"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    language_code: Mapped[str] = mapped_column(unique=True)
+    language_name: Mapped[str] = mapped_column()
